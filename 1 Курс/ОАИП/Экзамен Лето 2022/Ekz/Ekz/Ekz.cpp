2a570a73cde3041ac06c2e64a31146c389da2d78
DESKTOP-843OUJ2\Жмых <aisek-klark@yandex.com>
2022-06-14 13:56:21 +0300

    Экзамен по ОАиП

---
+++ 1 Курс/ОАИП/Экзамен Лето 2022/Ekz/Ekz/Ekz.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    char arr[] = "asdasd aaaaa aaa aaaaaaaaa";
+    cout << "Vvedite stroky: ";
+    cin.getline(arr, 256);
+    char* p;
+    int kol, num = 0, n = 0;
+
+    p = strtok(arr, " ");
+    kol = strlen(p);
+
+
+    while (p != NULL)
+    {
+        n++;
+        if (strlen(p) < kol)
+        {
+            kol = strlen(p);
+            num = n;
+        }
+        p = strtok(NULL, " ");
+    }
+    cout << "Poradkovii nomer slova " << num << "\n";
+    cout << "kol bukv " << kol << "\n";
+    
+    /*for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i];
+    }*/
+
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
---
+++ 1 Курс/ОАИП/Экзамен Лето 2022/Ekz_Test/Ekz_Test/Ekz_Test.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+
+
+void main() {
+    setlocale(LC_ALL, "rus");
+    int line, column, nomm = 0, nom = 0;
+
+    cout << "Введите размер массива: ";
+    cin >> line >> column;
+    int** array_a = new int* [line];
+    for (int i = 0; i < line; i++)
+        array_a[i] = new int[column];
+
+    cout << "Заполните массив" << endl;
+    for (int i = 0; i < line; i++)
+        for (int j = 0; j < column; j++)
+            cin >> array_a[i][j];
+
+    cout << "Массив:" << endl;
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < column; j++)
+            cout << array_a[i][j];
+        cout << endl;
+    }
+
+    int minLine = 0, maxLine = 0;
+    int minVal = 0, maxVal = 0;
+    for (int i = 0; i < line; i++)
+    {
+        for (int k = 0; k < column; k++)
+        {
+            if (array_a[i][k] < minVal)
+            {
+                minLine = k;
+            }
+
+            if (array_a[i][k] > maxVal)
+            {
+                maxLine = k;
+            }
+        }
+    }
+
+    int replace = 0;
+    for (int i = 0; i < line; i++)
+    {
+        swap(array_a[minLine][i], array_a[maxLine][i]);
+    }
+
+    cout << "Массив после обработки:" << endl;
+    for (int i = 0; i < line; i++) {
+        for (int j = 0; j < column; j++)
+            cout << array_a[i][j];
+        cout << endl;
+    }
+
+    cout << maxLine << "  " << minLine << endl;
+}
